becd65ee2f40a20bc81a3782f2de5a4e27e080d7
ok66ym <00okym02@gmail.com>
2026-03-26 01:06:22 +0900

    nvimのkeymap設定を修正

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -7,4 +7,8 @@ vim.keymap.set('i', 'jj', '<Esc>', { silent = true })
 if vim.g.vscode then
     -- 例: スペース + f でファイル検索を開く(VSCodeの機能)
     -- vim.keymap.set('n', '<Leader>f', "<cmd>call VSCodeNotify('workbench.action.quickOpen')<CR>")
+    -- 挿入モード(i)で jj を Esc にマッピング
+    vim.keymap.set('i', 'jj', '<Esc>', { noremap = true, silent = true })
+    -- VSCodeでjjをEscにマッピングするには、以下の遅延時間が限界
+    vim.opt.timeoutlen = 500
 end
